dba9af45c9e1bb1488e2fd675b2d3d1470b5df67
Arstanov Abylaikhan <beerlover671@gmail.com>
2024-11-05 22:30:10 +0500

    Added func main

--- main.go
+++ main.go
@@ -43,3 +43,11 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
 }
+
+func main() {
+	http.HandleFunc(`/cafe`, mainHandle)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
